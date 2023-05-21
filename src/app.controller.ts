3c69cd855b17b4d1add4e773f97929a18f9a8ed9
drewAnlex <andrewroa01@gmail.com>
2023-05-21 15:57:05 -0400

    feat(basic): return building count

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('aedificium')
+  getAedificiumNumber(): number {
+    return this.appService.getAedificiumNumber();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getAedificiumNumber(): number {
+    return 1;
+  }
 }
