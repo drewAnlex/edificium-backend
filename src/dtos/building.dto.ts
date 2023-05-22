0949ae066bc61b38683ca209de45d1e612f1d846
drewAnlex <andrewroa01@gmail.com>
2023-05-21 21:32:25 -0400

    feat(building): update DTO

--- src/dtos/building.dto.ts
+++ src/dtos/building.dto.ts
@@ -7,3 +7,17 @@ export class CreateBuildingDto {
   readonly nApartments: number;
   readonly status: number;
 }
+export class UpdateBuildingDto {
+  readonly name?: string;
+  readonly country?: string;
+  readonly state?: string;
+  readonly city?: string;
+  readonly zone?: string;
+  readonly nApartments?: number;
+  readonly apartments?: any[];
+  readonly administrators?: any[];
+  readonly coOwners?: any[];
+  readonly bills?: any[];
+  readonly news?: any[];
+  readonly status?: number;
+}
