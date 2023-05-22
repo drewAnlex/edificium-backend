7809233a7814d8ede8b3c16f007394bf3d416136
drewAnlex <andrewroa01@gmail.com>
2023-05-21 21:24:12 -0400

    feat(building): DTO used to create a bulding

---
+++ src/dtos/building.dto.ts
@@ -0,0 +1,9 @@
+export class CreateBuildingDto {
+  readonly name: string;
+  readonly country: string;
+  readonly state: string;
+  readonly city: string;
+  readonly zone: string;
+  readonly nApartments: number;
+  readonly status: number;
+}
