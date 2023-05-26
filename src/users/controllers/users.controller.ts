5355e5d94e253a3c5856c2cffc2d09b5c41f8783
drewAnlex <andrewroa01@gmail.com>
2023-05-25 23:09:25 -0400

    feat(users): create end points

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -27,8 +27,26 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
+  @Get(':userId')
+  getUser(@Param('userId', ParseIntPipe) userId: number) {
+    return this.usersService.findOne(userId);
+  }
+
   @Post()
   createUser(@Body() payload: CreateUserDto) {
     return this.usersService.create(payload);
+  }
+
+  @Put(':userId')
+  updateUser(
+    @Param('userId', ParseIntPipe) userId: number,
+    @Body() payload: UpdateUserDto,
+  ) {
+    return this.usersService.update(userId, payload);
+  }
+
+  @Delete(':userId')
+  deleteUser(@Param('userId', ParseIntPipe) userId: number) {
+    return this.usersService.remove(userId);
   }
 }
