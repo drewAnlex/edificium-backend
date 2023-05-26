536913b40f0ee87af26d7fac8b68567ae5b8f5db
drewAnlex <andrewroa01@gmail.com>
2023-05-26 00:27:58 -0400

    feat(users): CoOwner model

--- src/users/entities/User.ts
+++ src/users/entities/User.ts
@@ -1,8 +1,19 @@
+import { Apartment } from 'src/buildings/entities/apartment';
+import { Building } from 'src/buildings/entities/building';
+
 export class User {
   id: number;
   name: string;
   email: string;
   password: string;
+  phone: string;
   role: string;
   status: number;
+}
+
+export class CoOwner extends User {
+  apartments: Apartment[];
+  penaltys: any[];
+  payments: any[];
+  idNumber: string;
 }
