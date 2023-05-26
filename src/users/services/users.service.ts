5461134c1c8c8ee446d9121e21eade2b0b8a201a
drewAnlex <andrewroa01@gmail.com>
2023-05-25 22:45:23 -0400

    feat(users): findAll method

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '../entities/user';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [
+    {
+      id: 1,
+      name: 'User name',
+      email: 'User email',
+      password: 'User password',
+      role: 'User role',
+      status: 1,
+    },
+  ];
+
+  findAll() {
+    return this.users;
+  }
+}
