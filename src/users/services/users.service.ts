12c95f5891b407b2bbb12ff0ac3f9395a3929443
drewAnlex <andrewroa01@gmail.com>
2023-05-25 23:03:46 -0400

    feat(users): user service findOne, update, remove, create methods

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { User } from '../entities/user';
 import { CreateUserDto, UpdateUserDto } from '../dtos/users.dto';
 
@@ -19,6 +19,10 @@ export class UsersService {
     return this.users;
   }
 
+  findOne(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+
   create(payload: CreateUserDto) {
     const newUser = {
       id: this.users.length + 1,
@@ -26,5 +30,26 @@ export class UsersService {
     };
     this.users.push(newUser);
     return newUser;
+  }
+
+  update(id: number, payload: UpdateUserDto) {
+    if (!this.findOne(id)) {
+      throw new NotFoundException(`User #${id} not found`);
+    }
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    this.users[userIndex] = {
+      ...this.users[userIndex],
+      ...payload,
+    };
+    return this.users[userIndex];
+  }
+
+  remove(id: number) {
+    if (!this.findOne(id)) {
+      throw new NotFoundException(`User #${id} not found`);
+    }
+    const index = this.users.findIndex((user) => user.id === id);
+    this.users.splice(index, 1);
+    return true;
   }
 }
