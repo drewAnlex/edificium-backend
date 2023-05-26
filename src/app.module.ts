45c4b8d5961d8ada7d3459c01fcc8d2342720524
drewAnlex <andrewroa01@gmail.com>
2023-05-25 22:28:38 -0400

    project reestructuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BuildingsController } from './buildings/controllers/buildings.controller';
-import { BuildingsService } from './buildings/services/buildings.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { BuildingsModule } from './buildings/buildings.module';
 
 @Module({
   imports: [BuildingsModule],
-  controllers: [AppController, BuildingsController, UsersController],
-  providers: [AppService, BuildingsService, UsersService],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
