a2a0fce30f82c6aac4c92562c94be4a04d0bbb59
drewAnlex <andrewroa01@gmail.com>
2023-05-27 23:35:04 -0400

    feat(buildings): Apartment CRUD

--- src/buildings/controllers/apartment.controller.ts
+++ src/buildings/controllers/apartment.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ApartmentsService } from '../services/apartments.service';
+import { CreateAparmentDTO, UpdateAparmentDTO } from '../dtos/apartment.dto';
 
 @Controller('apartment')
-export class ApartmentController {}
+export class ApartmentController {
+  constructor(private apartmentsService: ApartmentsService) {}
+
+  @Get()
+  getApartments() {
+    return this.apartmentsService.findAll();
+  }
+
+  @Get(':id')
+  getAparment(@Param('id', ParseIntPipe) id: number) {
+    return this.apartmentsService.findOne(id);
+  }
+
+  @Post()
+  createAparment(@Body() payload: CreateAparmentDTO) {
+    return this.apartmentsService.create(payload);
+  }
+
+  @Put(':id')
+  updateAparment(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateAparmentDTO,
+  ) {
+    return this.apartmentsService.update(id, payload);
+  }
+
+  @Delete(':id')
+  deleteAparment(@Param('id', ParseIntPipe) id: number) {
+    return this.apartmentsService.remove(id);
+  }
+}
--- src/buildings/services/apartments.service.ts
+++ src/buildings/services/apartments.service.ts
@@ -1,4 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateAparmentDTO, UpdateAparmentDTO } from '../dtos/apartment.dto';
 
 @Injectable()
-export class ApartmentsService {}
+export class ApartmentsService {
+  private apartments = [
+    {
+      id: 1,
+      identifier: 'A-101',
+      floor: 1,
+      share: 0.1,
+      balance: 0,
+      status: 1,
+    },
+  ];
+
+  findAll() {
+    return this.apartments;
+  }
+
+  findOne(id: number) {
+    return this.apartments.find((apartment) => apartment.id === id);
+  }
+
+  create(payload: CreateAparmentDTO) {
+    const newApartment = {
+      id: this.apartments.length + 1,
+      ...payload,
+    };
+    this.apartments.push(newApartment);
+    return newApartment;
+  }
+
+  update(id: number, payload: UpdateAparmentDTO) {
+    if (!this.findOne(id)) {
+      throw new NotFoundException(`Apartment #${id} not found`);
+    }
+    const index = this.apartments.findIndex((apartment) => apartment.id === id);
+    this.apartments[index] = {
+      ...this.apartments[index],
+      ...payload,
+    };
+    return this.apartments[index];
+  }
+
+  remove(id: number) {
+    if (!this.findOne(id)) {
+      throw new NotFoundException(`Apartment #${id} not found`);
+    }
+    const apartmentIndex = this.apartments.findIndex(
+      (apartment) => apartment.id === id,
+    );
+    this.apartments.splice(apartmentIndex, 1);
+    return true;
+  }
+}
