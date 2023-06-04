087616f737d3c704cb7f9d1c4eba5717f7019666
drewAnlex <roadev01@gmail.com>
2023-06-04 15:38:10 -0400

    feat(payments): Payment Entity and DTO

---
+++ src/payments/dtos/Payment.dto.ts
@@ -0,0 +1,31 @@
+import { IsNotEmpty, IsString, IsNumber, IsPositive } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+
+export class PaymentDTO implements Readonly<PaymentDTO> {
+  @IsNotEmpty()
+  @IsNumber()
+  IndividualBill: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  UserId: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  Status: number;
+
+  @IsNotEmpty()
+  @IsString()
+  PayCode: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  Amount: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  MethodId: number;
+}
+
+export class PaymentUpdateDTO extends PartialType(PaymentDTO) {}
---
+++ src/payments/entities/Payment.ts
@@ -0,0 +1,9 @@
+export class Payment {
+  id: number;
+  IndividualBill: number;
+  UserId: number;
+  Status: number;
+  PayCode: string;
+  Amount: number;
+  MethodId: number;
+}
