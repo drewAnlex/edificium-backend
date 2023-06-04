dd05ed997567de4ddb54dc0972adfdd236bfd618
drewAnlex <roadev01@gmail.com>
2023-06-04 13:05:56 -0400

    feat(payment): Product Entity and DTO

---
+++ src/payments/dtos/Product.dto.ts
@@ -0,0 +1,34 @@
+import { IsNotEmpty, IsString, IsNumber, IsBoolean } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+
+export class CreateProductDto {
+  @IsNotEmpty()
+  @IsNumber()
+  BuildingBillsID: number;
+
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  price: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  quantity: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  suplierId: number;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  IsPaid: boolean;
+}
+
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
---
+++ src/payments/entities/Product.ts
@@ -0,0 +1,10 @@
+export class Product {
+  Id: number;
+  BuildingBillsID: number;
+  name: string;
+  description: string;
+  price: number;
+  quantity: number;
+  suplierId: number;
+  IsPaid: boolean;
+}
