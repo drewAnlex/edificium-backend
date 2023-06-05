715b11071cff1660a1f989f60d0325fe5046266d
drewAnlex <roadev01@gmail.com>
2023-06-05 12:53:59 -0400

    feat(paymentMethod): Payment Method List Entity and DTO

---
+++ src/payment-method/dtos/payment-method-list.dto.ts
@@ -0,0 +1,23 @@
+import { IsNotEmpty, IsNumber, IsPositive } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+
+export class PaymentMethodListDto implements Readonly<PaymentMethodListDto> {
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  MethodId: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  BuildingId: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  Status: number;
+}
+
+export class PartialPaymentMethodListDto extends PartialType(
+  PaymentMethodListDto,
+) {}
---
+++ src/payment-method/entities/payment-method-list.ts
@@ -0,0 +1,6 @@
+export class PaymentMethodList {
+  id: number;
+  MethodId: number;
+  BuildingId: number;
+  Status: number;
+}
