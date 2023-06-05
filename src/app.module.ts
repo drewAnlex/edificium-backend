78138d5044f3c4ac5766a8fb84450063b7adcdbd
drewAnlex <roadev01@gmail.com>
2023-06-05 12:12:38 -0400

    feat(paymentMethod): PaymentMethod Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { BuildingsModule } from './buildings/buildings.module';
 import { UsersModule } from './users/users.module';
 import { PaymentsModule } from './payments/payments.module';
+import { PaymentMethodModule } from './payment-method.module';
 
 @Module({
-  imports: [BuildingsModule, UsersModule, PaymentsModule],
+  imports: [BuildingsModule, UsersModule, PaymentsModule, PaymentMethodModule],
   controllers: [AppController],
   providers: [AppService],
 })
