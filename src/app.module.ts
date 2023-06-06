6367bfa6b061af79685a14ddc6b62e9d737619e2
drewAnlex <roadev01@gmail.com>
2023-06-06 13:34:23 -0400

    feat: config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,22 @@ import { UsersModule } from './users/users.module';
 import { PaymentsModule } from './payments/payments.module';
 import { PaymentMethodModule } from './payment-method/payment-method.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+
+import { environment } from './environments';
 
 @Module({
-  imports: [BuildingsModule, UsersModule, PaymentsModule, PaymentMethodModule, DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: environment[process.env.NODE_ENV] || '.env',
+      isGlobal: true,
+    }),
+    BuildingsModule,
+    UsersModule,
+    PaymentsModule,
+    PaymentMethodModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
