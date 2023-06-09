a399446ccf6529e847c446980766f92a7f156ab5
drewAnlex <roadev01@gmail.com>
2023-06-09 00:49:54 -0400

    feat(database): config and client connection

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,5 +1,28 @@
 import { Module, Global } from '@nestjs/common';
+import { Client } from 'pg';
+import config from 'src/config';
+import { ConfigType } from '@nestjs/config';
 
 @Global()
-@Module({})
+@Module({
+  providers: [
+    {
+      provide: 'PG',
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { user, password, name, port, host } = configService.database;
+        const client = new Client({
+          user,
+          host,
+          database: name,
+          password,
+          port,
+        });
+        client.connect();
+        return client;
+      },
+      inject: [config.KEY],
+    },
+  ],
+  exports: ['PG'],
+})
 export class DatabaseModule {}
