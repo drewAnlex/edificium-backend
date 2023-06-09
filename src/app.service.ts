65ace9d1e01df22874adb78adaae3120b6a8eb3f
drewAnlex <roadev01@gmail.com>
2023-06-09 00:50:27 -0400

    feat: count rows on buildings table

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,26 @@
 import { Injectable, Inject } from '@nestjs/common';
 import config from './config';
 import { ConfigType } from '@nestjs/config';
+import { Client } from 'pg';
 
 @Injectable()
 export class AppService {
   constructor(
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
+    @Inject('PG') private clientPg: Client,
   ) {}
   getHello(): string {
     return this.configService.misc.welcomeMessage;
   }
 
-  getAedificiumNumber(): number {
-    return 1;
+  getAedificiumNumber() {
+    return new Promise((resolve, reject) => {
+      this.clientPg.query('SELECT COUNT(*) FROM buildings', (err, res) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(res.rows[0].count);
+      });
+    });
   }
 }
