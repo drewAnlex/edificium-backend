70709dfb8c6ae1dbb50be50943ef04a76c01db7c
drewAnlex <roadev01@gmail.com>
2023-06-10 00:54:24 -0400

    fix:(users): typo

--- src/users/entities/Role.entity.ts
+++ src/users/entities/Role.entity.ts
@@ -3,7 +3,7 @@ import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
 @Entity()
 export class Role {
   @PrimaryGeneratedColumn()
-  Id: number;
+  id: number;
 
   @Column({ type: 'varchar', length: 32, unique: true })
   Name: string;
