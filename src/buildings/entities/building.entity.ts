9e7f345ce8e157904f3aa8436bb2aa59879ca6e2
drewAnlex <roadev01@gmail.com>
2023-06-11 01:17:40 -0400

    feat(buildings): TypeOrm setup

--- src/buildings/entities/building.entity.ts
+++ src/buildings/entities/building.entity.ts
@@ -1,15 +1,28 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Building {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 32, nullable: false })
   name: string;
+
+  @Column({ type: 'varchar', length: 32, nullable: false })
   country: string;
+
+  @Column({ type: 'varchar', length: 32, nullable: false })
   state: string;
+
+  @Column({ type: 'varchar', length: 32, nullable: false })
   city: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
   zone: string;
+
+  @Column({ type: 'int' })
   nApartments: number;
-  apartments: any[];
-  administrators: any[];
-  coOwners: any[];
-  bills: any[];
-  news: any[];
+
+  @Column({ type: 'int' })
   status: number;
 }
