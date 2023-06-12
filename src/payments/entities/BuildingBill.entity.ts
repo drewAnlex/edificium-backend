33a8392cf6ee3ee27b0512ac3f6a0733e359893e
drewAnlex <roadev01@gmail.com>
2023-06-12 14:09:38 -0400

    feat(payments): Building Bill with TypeORM

--- src/payments/entities/BuildingBill.entity.ts
+++ src/payments/entities/BuildingBill.entity.ts
@@ -1,9 +1,27 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { Building } from '../../buildings/entities/building.entity';
+import { User } from '../../users/entities/user.entity';
+
+@Entity()
 export class BuildingBill {
-  Id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Building)
   buildingId: number;
+
+  @ManyToOne(() => User)
   userId: number;
+
+  @Column({ type: 'varchar', length: 64 })
   name: string;
+
+  @Column({ type: 'text' })
   description: string;
+
+  @Column({ type: 'float' })
   balance: number;
+
+  @Column({ type: 'float' })
   total: number;
 }
--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -3,6 +3,7 @@ import { ServicesService } from './services/services.service';
 import { ServicesController } from './controllers/services.controller';
 import { ProductsService } from './services/products.service';
 import { ProductsController } from './controllers/products.controller';
+import { BuildingBillsService } from './services/building-bills.service';
 import { IndividualBillsService } from './services/individual-bills.service';
 import { IndividualBillsController } from './controllers/individual-bills.controller';
 import { PaymentsService } from './services/payments.service';
@@ -10,8 +11,27 @@ import { PaymentsController } from './controllers/payments.controller';
 import { PaymentInfoService } from './services/payment-info.service';
 import { PaymentInfoController } from './controllers/payment-info.controller';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BuildingBill } from './entities/BuildingBill.entity';
+import { BuildingBillsController } from './controllers/building-bills.controller';
+
 @Module({
-  providers: [ServicesService, ProductsService, IndividualBillsService, PaymentsService, PaymentInfoService],
-  controllers: [ServicesController, ProductsController, IndividualBillsController, PaymentsController, PaymentInfoController]
+  imports: [TypeOrmModule.forFeature([BuildingBill])],
+  providers: [
+    ServicesService,
+    ProductsService,
+    BuildingBillsService,
+    IndividualBillsService,
+    PaymentsService,
+    PaymentInfoService,
+  ],
+  controllers: [
+    ServicesController,
+    ProductsController,
+    BuildingBillsController,
+    IndividualBillsController,
+    PaymentsController,
+    PaymentInfoController,
+  ],
 })
 export class PaymentsModule {}
