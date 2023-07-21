a8d47a5cd05316763fdb6851a68eaf3ca6b96614
drewAnlex <roadev01@gmail.com>
2023-07-21 11:41:25 -0400

    feat(payment): Entity added

--- src/payments/entities/Payment.entity.ts
+++ src/payments/entities/Payment.entity.ts
@@ -1,9 +1,36 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { User } from 'src/users/entities/User.entity';
+import { IndividualBill } from './IndividualBill.entity';
+import { PaymentMethod } from '../../payment-method/entities/PaymentMethod.entity';
+
+@Entity()
 export class Payment {
+  @PrimaryGeneratedColumn()
   id: number;
-  IndividualBill: number;
-  UserId: number;
+
+  @OneToOne(() => IndividualBill, (individualBill) => individualBill.payment)
+  IndividualBill: IndividualBill;
+
+  @ManyToOne(() => User)
+  UserId: User;
+
+  @Column({ type: 'int' })
   Status: number;
+
+  @Column({ type: 'varchar', length: 32 })
   PayCode: string;
+
+  @Column({ type: 'float' })
   Amount: number;
-  MethodId: number;
+
+  @ManyToOne(() => PaymentMethod, (paymentMethod) => paymentMethod.payments)
+  @JoinColumn()
+  Method: PaymentMethod;
 }
