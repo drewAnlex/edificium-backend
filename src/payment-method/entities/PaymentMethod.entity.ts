bcb214ac45e3619de8b42e6d80e0f39b2325b0d0
drewAnlex <roadev01@gmail.com>
2023-07-21 13:24:50 -0400

    feat(paymentMethods): added fields

--- src/payment-method/entities/PaymentMethod.entity.ts
+++ src/payment-method/entities/PaymentMethod.entity.ts
@@ -1,4 +1,13 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Payment } from '../../payments/entities/Payment.entity';
+import { PaymentInfo } from 'src/payments/entities/payment-info.entity';
 
 @Entity()
 export class PaymentMethod {
@@ -10,4 +19,16 @@ export class PaymentMethod {
 
   @Column({ type: 'int' })
   status: number;
+
+  @OneToMany(() => Payment, (payment) => payment.Method)
+  payments: Payment[];
+
+  @OneToMany(() => PaymentInfo, (paymentInfo) => paymentInfo.methodId)
+  paymentInfos: PaymentInfo[];
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
 }
