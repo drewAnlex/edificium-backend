c02df2fab17284ab1b6b9342856ed7185fd2c8ae
drewAnlex <roadev01@gmail.com>
2023-07-21 11:58:06 -0400

    feat(payment): Fields added

--- src/payments/entities/Payment.entity.ts
+++ src/payments/entities/Payment.entity.ts
@@ -4,11 +4,15 @@ import {
   Column,
   OneToOne,
   ManyToOne,
+  OneToMany,
   JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 import { User } from 'src/users/entities/User.entity';
 import { IndividualBill } from './IndividualBill.entity';
 import { PaymentMethod } from '../../payment-method/entities/PaymentMethod.entity';
+import { PaymentInfo } from './payment-info.entity';
 
 @Entity()
 export class Payment {
@@ -33,4 +37,13 @@ export class Payment {
   @ManyToOne(() => PaymentMethod, (paymentMethod) => paymentMethod.payments)
   @JoinColumn()
   Method: PaymentMethod;
+
+  @OneToMany(() => PaymentInfo, (paymentInfo) => paymentInfo.payment)
+  paymentInfos: PaymentInfo[];
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
 }
