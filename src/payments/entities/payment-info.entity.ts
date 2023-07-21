4a35661a5ee88134aedefa20b608da429bf26cbb
drewAnlex <roadev01@gmail.com>
2023-07-21 11:57:50 -0400

    feat(payment): PaymentInfo Entity Added

--- src/payments/entities/payment-info.entity.ts
+++ src/payments/entities/payment-info.entity.ts
@@ -1,7 +1,37 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { PaymentMethod } from 'src/payment-method/entities/PaymentMethod.entity';
+import { Payment } from './Payment.entity';
+
+@Entity()
 export class PaymentInfo {
+  @PrimaryGeneratedColumn()
   id: number;
-  methodId: number;
-  paymentId: number;
+
+  @ManyToOne(() => PaymentMethod, (paymentMethod) => paymentMethod.paymentInfos)
+  @JoinColumn()
+  methodId: PaymentMethod;
+
+  @ManyToOne(() => Payment, (payment) => payment.paymentInfos)
+  @JoinColumn()
+  payment: Payment;
+
+  @Column({ type: 'varchar', length: 32 })
   name: string;
+
+  @Column({ type: 'varchar', length: 32 })
   value: string;
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
 }
