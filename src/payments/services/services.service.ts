dfbf754ffd0608baa43ec73f043535d00a7932da
drewAnlex <andrewroa01@gmail.com>
2023-09-05 23:23:49 -0400

    feat(BB): add service price to BB on create

--- src/payments/services/services.service.ts
+++ src/payments/services/services.service.ts
@@ -9,11 +9,13 @@ import { CreateServiceDTO, UpdateServiceDTO } from '../dtos/Service.dto';
 
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { BuildingBillsService } from './building-bills.service';
 
 @Injectable()
 export class ServicesService {
   constructor(
     @InjectRepository(Service) private serviceRepo: Repository<Service>,
+    private bbService: BuildingBillsService,
   ) {}
 
   findAll() {
@@ -35,8 +37,11 @@ export class ServicesService {
 
   async create(payload: CreateServiceDTO) {
     const newService = this.serviceRepo.create(payload);
+    const bill = await this.bbService.findOne(payload.buildingBillId.id);
+    bill.total += payload.price;
     try {
       await this.serviceRepo.save(newService);
+      await this.bbService.update(bill.id, bill);
     } catch (error) {
       throw new HttpException(`Error ${error}`, HttpStatus.BAD_REQUEST);
     }
