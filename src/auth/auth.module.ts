e43bd6c47c0e46467bdb5861b51741189b4eb13c
drewAnlex <andrewroa01@gmail.com>
2023-09-17 11:52:29 -0400

    feat: JWT and Passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,28 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './services/auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users/users.module';
+import { LocalStrategy } from './strategies/local.strategy';
+import { AuthController } from './controllers/auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import config from '../config';
+import { ConfigType } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [
+    PassportModule,
+    UsersModule,
+    JwtModule.registerAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => ({
+        secret: configService.jwt.secret,
+        signOptions: {
+          expiresIn: configService.jwt.expiresIn,
+        },
+      }),
+    }),
+  ],
+  providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
+})
 export class AuthModule {}
