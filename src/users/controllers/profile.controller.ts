6647ebc2c199127edfb245a56706d1dbb0cbec2d
drewAnlex <andrewroa01@gmail.com>
2023-09-17 21:36:32 -0400

    feat: profile controller

--- src/users/controllers/profile.controller.ts
+++ src/users/controllers/profile.controller.ts
@@ -16,7 +16,6 @@ import { Roles } from 'src/auth/decorators/roles.decorator';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { Request } from 'express';
-import { PayloadToken } from 'src/auth/models/token.model';
 
 @ApiTags('profile')
 @UseGuards(JwtAuthGuard, RolesGuard)
@@ -28,21 +27,22 @@ export class ProfileController {
   @Roles('Staff', 'Admin', 'User')
   @Get()
   getUser(@Req() req: Request) {
-    const user = req.user as PayloadToken;
-    return this.usersService.findOne(user.sub);
+    const user = req.user as any;
+    console.log(user);
+    return this.usersService.findOne(user.userId);
   }
 
   @Roles('Staff', 'Admin', 'User')
   @Put()
   updateUser(@Req() req: Request, @Body() payload: UpdateUserDto) {
-    const user = req.user as PayloadToken;
-    return this.usersService.update(user.sub, payload);
+    const user = req.user as any;
+    return this.usersService.update(user.userId, payload);
   }
 
   @Roles('Staff', 'Admin', 'User')
   @Delete()
   deleteUser(@Req() req: Request) {
-    const user = req.user as PayloadToken;
-    return this.usersService.remove(user.sub);
+    const user = req.user as any;
+    return this.usersService.remove(user.userId);
   }
 }
